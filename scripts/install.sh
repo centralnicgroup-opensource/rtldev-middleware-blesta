1693f28d3de439b4100424304061cbbb5f3193b1
Asif Nawaz <asif.nawaz@centralnic.com>
2022-11-24 10:59:33 +0000

    ci(workflows): test and release workflow reconfigured

--- scripts/install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo cp -R ~/git/blesta-ispapi-registrar/components/modules/ispapi /var/www/blesta/components/modules
-sudo chown -R www-data.www-data /var/www/blesta/components/modules/ispapi
