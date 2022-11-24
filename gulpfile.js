ca55ce512cbf993bb953780efdf4514a7bd73bd1
Kai Schwarz <kschwarz@hexonet.net>
2022-11-24 16:50:37 +0100

    fix(archives): add missing software dependency SDK

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,33 @@
-const { series, src, dest } = require('gulp')
-const clean = require('gulp-clean')
-const zip = require('gulp-zip')
-const exec = require('util').promisify(require('child_process').exec)
-const cfg = require('./gulpfile.json')
+const { series, src, dest } = require("gulp");
+const clean = require("gulp-clean");
+const zip = require("gulp-zip");
+const exec = require("util").promisify(require("child_process").exec);
+const eosp = require("end-of-stream-promise");
+const composer = require("gulp-composer");
+const cfg = require("./gulpfile.json");
+
+/**
+ * Perform composer update
+ * @return stream
+ */
+async function doComposerUpdate() {
+  try {
+    await exec(`rm -rf ${cfg.vendorFolder}`);
+  } catch (e) {}
+  await eosp(composer("validate"));
+  await eosp(composer("update --no-dev"));
+}
 
 /**
  * cleanup old build folder / archive
  * @return stream
  */
 function doDistClean() {
-  return src([cfg.archiveBuildPath, `${cfg.archiveFileName}-latest.zip`], { read: false, base: '.', allowEmpty: true })
-    .pipe(clean({ force: true }))
+  return src([cfg.archiveBuildPath, `${cfg.archiveFileName}-latest.zip`], {
+    read: false,
+    base: ".",
+    allowEmpty: true,
+  }).pipe(clean({ force: true }));
 }
 
 /**
@@ -18,8 +35,9 @@ function doDistClean() {
  * @return stream
  */
 function doCopyFiles() {
-  return src(cfg.filesForArchive, { base: '.' })
-    .pipe(dest(cfg.archiveBuildPath))
+  return src(cfg.filesForArchive, { base: "." }).pipe(
+    dest(cfg.archiveBuildPath)
+  );
 }
 
 /**
@@ -27,8 +45,11 @@ function doCopyFiles() {
  * @return stream
  */
 function doFullClean() {
-  return src(cfg.filesForCleanup, { read: false, base: '.', allowEmpty: true })
-    .pipe(clean({ force: true }))
+  return src(cfg.filesForCleanup, {
+    read: false,
+    base: ".",
+    allowEmpty: true,
+  }).pipe(clean({ force: true }));
 }
 
 /**
@@ -38,7 +59,7 @@ function doFullClean() {
 function doGitZip() {
   return src(`./${cfg.archiveBuildPath}/**`)
     .pipe(zip(`${cfg.archiveFileName}-latest.zip`))
-    .pipe(dest('.'))
+    .pipe(dest("."));
 }
 
 /**
@@ -48,21 +69,11 @@ function doGitZip() {
 function doZip() {
   return src(`./${cfg.archiveBuildPath}/**`)
     .pipe(zip(`${cfg.archiveFileName}.zip`))
-    .pipe(dest('./pkg'))
+    .pipe(dest("./pkg"));
 }
 
-exports.copy = series(
-  doDistClean,
-  doCopyFiles
-)
+exports.copy = series(doComposerUpdate, doDistClean, doCopyFiles);
 
-exports.archives = series(
-  doGitZip,
-  doZip
-)
+exports.archives = series(doGitZip, doZip);
 
-exports.release = series(
-  exports.copy,
-  exports.archives,
-  doFullClean
-)
+exports.release = series(exports.copy, exports.archives, doFullClean);
